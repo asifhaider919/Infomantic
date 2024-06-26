232651f2de598f892c6cf54ff2b3cbd78f42219c
asifhaider919 <asifhaider.rno@gmail.com>
2024-06-26 23:01:25 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,8 +10,11 @@ st.title("Site Map")
 uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
 
 if uploaded_file is not None:
-    # Read the uploaded CSV file
-    data = pd.read_csv(uploaded_file)
+    try:
+        # Read the uploaded CSV file
+        data = pd.read_csv(uploaded_file, encoding='utf-8')
+    except UnicodeDecodeError:
+        data = pd.read_csv(uploaded_file, encoding='ISO-8859-1')
 
     # Display the data
     st.write("Data preview:")
