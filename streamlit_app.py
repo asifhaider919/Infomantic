b0b52ff6b81dff99352f5aca0e019aa7cc093603
asifhaider919 <asifhaider.rno@gmail.com>
2024-06-30 04:14:21 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ if uploaded_file is not None:
         else:
             # Define categories for the legend based on 'Issue' column
             categories = data['Issue'].unique().tolist()
-            colors = ['blue', 'red', 'green', 'orange', 'purple']  # Adjust colors as needed
+            colors = ['blue', 'red', 'green', 'orange', 'purple', 'black']  # Adjust colors as needed
 
             # Sidebar filter by Site Name
             st.sidebar.subheader("Filter by Site Name")
