4f0ae74e21bfdd7e80dbd48d80fb284b589ca83d
asifhaider919 <asifhaider.rno@gmail.com>
2024-06-30 00:16:21 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,10 +41,10 @@ if uploaded_file is not None:
                 legend=dict(orientation='h', yanchor='top', y=1.1, xanchor='left', x=0.5),  # Legend position
                 xaxis=dict(showgrid=False, zeroline=False),  # Hide gridlines and zeroline
                 yaxis=dict(showgrid=False, zeroline=False),  # Hide gridlines and zeroline
-                plot_bordercolor='lightgray',   # Set plot area border color
-                plot_borderwidth=1              # Set plot area border width
+                plot_bgcolor='rgba(0,0,0,0)',  # Make plot area transparent
+                plot_bordercolor='lightgray',  # Set plot area border color
+                plot_borderwidth=1,            # Set plot area border width
             )
             st.plotly_chart(fig)
     else:
         st.error("'items' column not found in the uploaded file. Please check the column names.")
-
