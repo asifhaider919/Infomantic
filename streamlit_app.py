8e9ecc1bc2cfa475a46ebe5a7adf97a6997768f5
asifhaider919 <asifhaider.rno@gmail.com>
2024-06-30 01:38:10 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 from datetime import datetime
+import fnmatch
 
 # Set wide layout
 st.set_page_config(layout="wide")
@@ -20,77 +21,100 @@ if uploaded_file is not None:
     # Load the Excel file
     df = pd.read_excel(uploaded_file)
 
-    # Convert the DateTime column to pandas datetime type if available
-    if 'DateTime' in df.columns:
-        df['DateTime'] = pd.to_datetime(df['DateTime'])
-        date_range = (df['DateTime'].min(), df['DateTime'].max())
-    else:
-        st.sidebar.error("DateTime column not found in the uploaded file.")
+    # Convert the DateTime column to pandas datetime type
+    df['DateTime'] = pd.to_datetime(df['DateTime'])
 
     # Sidebar for controlling chart dimensions
     st.sidebar.header("Chart Settings")
     chart_width = st.sidebar.slider("Chart Width", min_value=500, max_value=3000, value=800)
     chart_height = st.sidebar.slider("Chart Height", min_value=300, max_value=1000, value=default_chart_height)
 
+    # Determine date range from DataFrame
+    if 'DateTime' in df.columns:
+        date_range = (df['DateTime'].min(), df['DateTime'].max())
+
     # DateTime slider in the sidebar
     if date_range:
         start_date, end_date = st.sidebar.slider(
             "Select Date Range",
-            min_value=date_range[0].date(),
-            max_value=date_range[1].date(),
-            value=(date_range[0].date(), date_range[1].date())
+            min_value=datetime.date(date_range[0]),
+            max_value=datetime.date(date_range[1]),
+            value=(datetime.date(date_range[0]), datetime.date(date_range[1]))
         )
 
         # Convert start_date and end_date to datetime64[ns]
         start_date = pd.to_datetime(start_date)
         end_date = pd.to_datetime(end_date)
 
-    # Sidebar for filtering metrics
-    st.sidebar.header("Filter Metrics")
-
-    # Get unique metrics assuming they start from the third column
-    all_metrics = df.columns[2:].tolist() if 'DateTime' in df.columns else []
-
-    if len(all_metrics) > 0:
-        # Multiselect dropdown for selecting metrics
-        selected_metrics = st.sidebar.multiselect(
-            "Select Metrics",
-            all_metrics,
-            default=all_metrics,
-            format_func=lambda x: "All Metrics" if x == all_metrics else x,
-            key="metrics_multiselect"
-        )
-
-        # Filtered DataFrame based on selected metrics
-        if selected_metrics:
-            filtered_df = df[['DateTime'] + selected_metrics]  # Ensure DateTime column is always included
-        else:
-            st.error("Please select at least one metric.")
-
-        # Display charts for selected metrics
-        if filtered_df is not None:
-            for metric in selected_metrics:
-                if metric != 'DateTime':  # Skip plotting for DateTime column
-                    fig = px.line(filtered_df, x='DateTime', y=metric, labels={'DateTime': 'Date', metric: metric})
-                    fig.update_layout(
-                        xaxis_title='',
-                        yaxis_title='',
-                        width=chart_width,
-                        height=chart_height,
-                        margin=dict(l=0, r=40, t=0, b=0),  # Set margin to 40px on the right
-                        paper_bgcolor='rgb(240, 240, 240)',  # Set paper background color to a lighter gray (RGB values)
-                        plot_bgcolor='rgba(0,0,0,0)',   # Make plot area transparent
-                        legend=dict(
-                            orientation='h',  # Horizontal orientation
-                            yanchor='bottom',  # Anchor legend to the bottom of the plot area
-                            y=1.02,  # Adjust vertical position
-                            xanchor='right',  # Anchor legend to the right of the plot area
-                            x=1  # Adjust horizontal position
-                        ),
-                        xaxis=dict(showgrid=False, zeroline=False),  # Hide gridlines and zeroline
-                        yaxis=dict(showgrid=False, zeroline=False),  # Hide gridlines and zeroline
-                    )
-                    st.plotly_chart(fig)
-
     else:
-        st.warning("No metrics found in the uploaded file.")
+        st.sidebar.warning("No DateTime column found in the uploaded file.")
+
+    # Ensure the 'items' column exists
+    if 'items' in df.columns:
+        # Multiselect dropdown for selecting metrics
+        all_metrics_option = "All Metrics"
+        available_metrics = df.columns[2:].tolist()
+
+        # Checkbox to display all metrics
+        display_all_metrics = st.sidebar.checkbox("Display All Metrics")
+
+        if display_all_metrics:
+            selected_metrics = available_metrics
+        else:
+            # Search box for filtering metrics by typing
+            filter_text = st.sidebar.text_input("Filter Metrics", "")
+
+            # Filter metrics for autocomplete suggestions
+            filtered_metrics = [metric for metric in available_metrics if fnmatch.fnmatch(metric.lower(), f'*{filter_text.lower()}*')]
+
+            # Show autocomplete suggestions in a selectbox
+            selected_metric = st.sidebar.selectbox("Select Metric", [""] + filtered_metrics)
+
+            # Handle selection of metrics
+            if selected_metric:
+                selected_metrics = [selected_metric]
+            else:
+                selected_metrics = []  # No metric selected
+
+        if len(selected_metrics) > 0:
+            # Create two columns for displaying charts side by side
+            col1, col2 = st.columns(2)
+
+            # Iterate through each selected metric
+            for i, col in enumerate(selected_metrics, start=1):
+                if col == all_metrics_option:
+                    continue  # Skip "All Metrics" option in individual charts
+
+                # Filter data based on selected date range and metric
+                filtered_df = df[(df['DateTime'] >= start_date) & (df['DateTime'] <= end_date)]
+
+                # Create an interactive plot using Plotly for each metric
+                fig = px.line(filtered_df, x='DateTime', y=col, color='items', labels={'items': col})  # Use column name as legend
+                fig.update_layout(
+                    xaxis_title='',
+                    yaxis_title='',
+                    width=chart_width,
+                    height=chart_height,
+                    margin=dict(l=0, r=40, t=0, b=0),  # Set margin to 40px on the right
+                    paper_bgcolor='rgb(240, 240, 240)',  # Set paper background color to a lighter gray (RGB values)
+                    plot_bgcolor='rgba(0,0,0,0)',   # Make plot area transparent
+                    legend=dict(
+                        orientation='h',  # Horizontal orientation
+                        yanchor='bottom',  # Anchor legend to the bottom of the plot area
+                        y=1.02,  # Adjust vertical position
+                        xanchor='right',  # Anchor legend to the right of the plot area
+                        x=1  # Adjust horizontal position
+                    ),
+                    xaxis=dict(showgrid=False, zeroline=False),  # Hide gridlines and zeroline
+                    yaxis=dict(showgrid=False, zeroline=False),  # Hide gridlines and zeroline
+                )
+
+                # Alternate placing charts in col1 and col2
+                if i % 2 == 1:
+                    col1.plotly_chart(fig)
+                else:
+                    col2.plotly_chart(fig)
+        else:
+            st.warning("Please select at least one metric to display.")
+    else:
+        st.error("'items' column not found in the uploaded file. Please check the column names.")
