ec39f0615a759f76a691b3cb74408ca33c3d60bc
asifhaider919 <asifhaider.rno@gmail.com>
2024-06-30 01:22:27 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,4 +70,22 @@ if uploaded_file is not None:
                 height=chart_height,
                 margin=dict(l=0, r=40, t=0, b=0),  # Set margin to 40px on the right
                 paper_bgcolor='rgb(240, 240, 240)',  # Set paper background color to a lighter gray (RGB values)
-                plot_bgcolor='rgba(0,0,0,0)',   # Make plot ar
+                plot_bgcolor='rgba(0,0,0,0)',   # Make plot area transparent
+                legend=dict(
+                    orientation='h',  # Horizontal orientation
+                    yanchor='bottom',  # Anchor legend to the bottom of the plot area
+                    y=1.02,  # Adjust vertical position
+                    xanchor='right',  # Anchor legend to the right of the plot area
+                    x=1  # Adjust horizontal position
+                ),
+                xaxis=dict(showgrid=False, zeroline=False),  # Hide gridlines and zeroline
+                yaxis=dict(showgrid=False, zeroline=False),  # Hide gridlines and zeroline
+            )
+
+            # Alternate placing charts in col1 and col2
+            if i % 2 == 1:
+                col1.plotly_chart(fig)
+            else:
+                col2.plotly_chart(fig)
+    else:
+        st.error("'items' column not found in the uploaded file. Please check the column names.")
