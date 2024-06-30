c184aba7ee32499e7a5037868badb1ba7567a60c
asifhaider919 <asifhaider.rno@gmail.com>
2024-06-30 00:49:33 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,28 +58,4 @@ if uploaded_file is not None:
 
         # Iterate through each metric column (starting from the 3rd column)
         for i, col in enumerate(df.columns[2:], start=1):
-            # Filter data based on selected date range
-            filtered_df = df[(df['DateTime'] >= start_date) & (df['DateTime'] <= end_date)]
-
-            # Create an interactive plot using Plotly for each metric
-            fig = px.line(filtered_df, x='DateTime', y=col, color='items', labels={'items': col})  # Use column name as legend
-            fig.update_layout(
-                xaxis_title='',
-                yaxis_title='',
-                width=chart_width,
-                height=chart_height,
-                margin=dict(l=0, r=40, t=0, b=0),  # Set margin to 40px on the right
-                paper_bgcolor='rgb(240, 240, 240)',  # Set paper background color to a lighter gray (RGB values)
-                plot_bgcolor='rgba(0,0,0,0)',   # Make plot area transparent
-                legend=dict(orientation='h', yanchor='bottom', y=0.01, xanchor='left', x=0.5),  # Legend at the bottom
-                xaxis=dict(showgrid=False, zeroline=False),  # Hide gridlines and zeroline
-                yaxis=dict(showgrid=False, zeroline=False),  # Hide gridlines and zeroline
-            )
-
-            # Alternate placing charts in col1 and col2
-            if i % 2 == 1:
-                col1.plotly_chart(fig)
-            else:
-                col2.plotly_chart(fig)
-    else:
-        st.error("'items' column not found in the uploaded file. Please check the column names.")
+            # Filter data based on selec
